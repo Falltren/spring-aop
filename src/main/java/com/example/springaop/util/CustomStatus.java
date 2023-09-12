f04625a373edd801e02d93a0f1faa526801196c3
Andrei Guzei <aaguzey@gmail.com>
2023-09-12 17:48:33 +1000

    Added error status

---
+++ src/main/java/com/example/springaop/util/CustomStatus.java
@@ -0,0 +1,15 @@
+package com.example.springaop.util;
+
+public enum CustomStatus {
+    SUCCESS(0, "Success"),
+    NOT_FOUND(1, "Not found"),
+    EXCEPTION(2, "Exception");
+
+    private final int code;
+    private final String message;
+
+    CustomStatus(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}

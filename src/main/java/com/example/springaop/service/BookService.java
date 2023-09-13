e8b6907aa27ae054170b71dc0e6ccdc1d72cf18e
Andrei Guzei <aaguzey@gmail.com>
2023-09-13 14:18:42 +1000

    Added aop for logging and exception handling

--- src/main/java/com/example/springaop/service/BookService.java
+++ src/main/java/com/example/springaop/service/BookService.java
@@ -23,44 +23,17 @@ public class BookService {
 
 
     public CustomResponse<Book> getAll() {
-        List<Book> books;
-        try {
-            log.info("Попытка получить все книги");
-            books = bookRepository.findAll();
-        }catch (Exception e){
-            log.error(e.getMessage(), e);
-            return new CustomResponse<>(null, CustomStatus.EXCEPTION);
-        }
-        log.info("Все книги получены");
+        List<Book> books = bookRepository.findAll();
         return new CustomResponse<>(books, CustomStatus.SUCCESS);
     }
 
     public CustomResponse<Book> getBookByTitle(String title) {
-        Book book;
-        try {
-            log.info("Попытка получить книгу с названием {}", title);
-            book = bookRepository.findByTitle(title).orElseThrow();
-        }catch (NoSuchElementException e){
-            log.error(e.getMessage(), e);
-            return new CustomResponse<>(null, CustomStatus.NOT_FOUND);
-        }catch (Exception e){
-            log.error(e.getMessage(), e);
-            return new CustomResponse<>(null, CustomStatus.EXCEPTION);
-        }
-        log.info("Книга с названием {} получена", title);
+        Book book = bookRepository.findByTitle(title).orElseThrow();
         return new CustomResponse<>(Stream.of(book).collect(Collectors.toList()), CustomStatus.SUCCESS);
     }
 
     public CustomResponse<Book> addBook(Book book) {
-        Book newBook;
-        try {
-            log.info("Попытка добавить книгу с названием {}", book.getTitle());
-            newBook =bookRepository.save(book);
-        }catch (Exception e){
-            log.error(e.getMessage(), e);
-            return new CustomResponse<>(null, CustomStatus.EXCEPTION);
-        }
-        log.info("Книга с названием {} добавлена", newBook.getTitle());
+        Book newBook = bookRepository.save(book);
         return new CustomResponse<>(Stream.of(newBook).collect(Collectors.toList()), CustomStatus.SUCCESS);
     }
 }
